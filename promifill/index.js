0514e4467609792efe36c885758896b1081c8160
Bruno Scopelliti <bru.scopelliti@gmail.com>
2018-01-06 06:31:04 +0100

    Init promise's state and value

--- promifill/index.js
+++ promifill/index.js
@@ -1,6 +1,11 @@
 "use strict";
 
+const [PENDING, FULFILLED, REJECTED] =
+  [void 0, true, false];
+
 class Promifill {
   constructor () {
+    this.state = PENDING;
+    this.value = void 0;
   }
 }
