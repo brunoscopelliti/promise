74059788167b45385b91b020bb72f62e9f722dc7
Bruno Scopelliti <bru.scopelliti@gmail.com>
2018-01-06 06:32:44 +0100

    Define constructor's parameter

--- promifill/index.js
+++ promifill/index.js
@@ -4,7 +4,11 @@ const [PENDING, FULFILLED, REJECTED] =
   [void 0, true, false];
 
 class Promifill {
-  constructor () {
+  constructor (executor) {
+    if (typeof executor != "function") {
+      throw new TypeError(`Promise resolver ${Object.prototype.toString.call(executor)} is not a function`);
+    }
+
     this.state = PENDING;
     this.value = void 0;
   }
