3e36f147e700840fd43da6ccf9ee3e6f18cbdfc5
Bruno Scopelliti <bru.scopelliti@gmail.com>
2018-01-18 06:19:09 +0100

    Implementation of schedule hardcoded on MutationObserver

--- promifill/scheduler/index.js
+++ promifill/scheduler/index.js
@@ -1,6 +1,32 @@
 const schedule =
-  (observers) => {
-    throw new Error("Not implemented.");
-  };
+  (() => {
+    let microtasks = [];
+
+    const run =
+      () => {
+        let handler, value;
+        while (microtasks.length > 0 && ({ handler, value } = microtasks.shift())) {
+          handler(value);
+        }
+      };
+
+    const observer = new MutationObserver(run);
+    const node = document.createTextNode("");
+
+    observer.observe(node, { characterData: true });
+
+    return (observers) => {
+      if (observers.length == 0) {
+        return;
+      }
+
+      microtasks = microtasks.concat(observers);
+      observers.length = 0;
+
+      node.data = node.data === 1
+        ? 0
+        : 1;
+    };
+  })();
 
 module.exports = schedule;
